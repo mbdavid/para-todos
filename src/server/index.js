b8ac0c34e2421210e4f20a186d56d180ba3115d0
mbdavid <mauricio.david@gmail.com>
2015-11-06 04:11:14 -0200

    Suporte a offline

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var cacheManifest = require('connect-cache-manifest');
 
 app.use(express.static('public'));
+
+app.use(cacheManifest({
+    manifestPath: '/app.manifest',
+    files: [{
+        dir: __dirname + '/../public/css',
+        prefix: '/css/'
+    }, {
+        dir: __dirname + '/../public/fonts',
+        prefix: '/fonts/'
+    }, {
+        dir: __dirname + '/../public/img',
+        prefix: '/img/'
+    }, {
+        dir: __dirname + '/../public/js',
+        prefix: '/js/'
+    }, {
+        dir: __dirname + '/../public',
+        prefix: '/'
+    }],
+    networks: ['*'],
+    fallbacks: []
+}));
+
 
 //app.get('/', function (req, res) {
 //  res.send('Hello World!');
